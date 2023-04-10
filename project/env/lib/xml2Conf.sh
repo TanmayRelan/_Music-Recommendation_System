5594e15a68944650429dbe69b2011a0bd69d3b66
Tanmay <tanmayrela@mac-2.local>
2023-04-10 19:22:57 +0530

    First_commit

---
+++ project/env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/tanmayrela/Desktop/Ai project/project/env/lib"
+XML2_LIBS="-lxml2 -L/Users/tanmayrela/Desktop/Ai project/project/env/lib -lz -L/Users/tanmayrela/Desktop/Ai project/project/env/lib -llzma -lpthread -L/Users/tanmayrela/Desktop/Ai project/project/env/lib -liconv -L/Users/tanmayrela/Desktop/Ai project/project/env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/Users/tanmayrela/Desktop/Ai project/project/env/include/libxml2 -I/Users/tanmayrela/Desktop/Ai project/project/env/include"
+MODULE_VERSION="xml2-2.9.14"
+
---
+++ project/env/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/tanmayrela/Desktop/Ai project/project/env/lib"
+XSLT_LIBS="-lxslt -L/Users/tanmayrela/Desktop/Ai project/project/env/lib -lxml2 -L/Users/tanmayrela/Desktop/Ai project/project/env/lib -L/Users/tanmayrela/Desktop/Ai project/project/env/lib -lpthread -L/Users/tanmayrela/Desktop/Ai project/project/env/lib -liconv -L/Users/tanmayrela/Desktop/Ai project/project/env/lib -licui18n -licuuc -licudata -lm "
+XSLT_PRIVATE_LIBS=""
+XSLT_INCLUDEDIR="-I/Users/tanmayrela/Desktop/Ai project/project/env/include"
+MODULE_VERSION="xslt-1.1.35"
